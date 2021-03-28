10f4bb66d9c9515ed60f65e6cf70ab53c1333038
Ömer uysal <teouysal@outlook.com>
2021-03-28 10:35:39 +0300

    added spring boot mongo file upload

---
+++ spring-mongoDB-FileUpload/src/main/java/com/example/demo/controller/FileUploadController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.demo.service.FileUploadService;
+
+@RestController
+@RequestMapping("/upload")
+public class FileUploadController {
+	
+	
+	   @Autowired
+	    private FileUploadService fileUploadService;
+
+	    @PostMapping
+	    public void fileUpload(@RequestParam MultipartFile file) throws Exception {
+	        fileUploadService.uploadFile(file);
+	    }
+
+}
---
+++ spring-mongoDB-FileUpload/src/main/java/com/example/demo/service/FileUploadService.java
@@ -0,0 +1,29 @@
+package com.example.demo.service;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+
+@Service
+public class FileUploadService {
+	
+	
+	   @Autowired
+	    private GridFsTemplate gridFsTemplate;
+
+	    public void uploadFile(MultipartFile file) throws Exception {
+	        DBObject dbObject = new BasicDBObject();
+	        dbObject.put("filename", file.getOriginalFilename());
+	        dbObject.put("contentType", file.getContentType());
+	        dbObject.put("size", file.getSize());
+	        dbObject.put("userId", "12345");
+	        ObjectId id = gridFsTemplate.store(file.getInputStream(),file.getOriginalFilename(), dbObject);
+	        System.out.println(id.toString());
+	    }
+
+}
